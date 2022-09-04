cf59666571a0dd2da5aca595ea7f99d5db1f8ce5
Kirill Dyakov <kira-d03@mail.ru>
2022-09-04 17:52:41 +0300

    Simple sending and getting done

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.example.chatapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.FirebaseDatabase
+import com.example.chatapp.databinding.ActivityMainBinding
+import com.google.firebase.database.*
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
+        binding.bSend.setOnClickListener {
+            myRef.setValue(binding.eMsg.text.toString())
+        }
+        onChangeListener(myRef)
+    }
 
-        myRef.setValue("Hello, World!")
+    private fun onChangeListener(dRef: DatabaseReference) {
+        dRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                binding.apply {
+                    tvMsg.append("\n")
+                    tvMsg.append(p0.value.toString())
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+        })
     }
 }
