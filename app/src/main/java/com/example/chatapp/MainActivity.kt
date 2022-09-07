4703be129782e5b363960c88682afbaa88c26a54
Kirill Dyakov <kira-d03@mail.ru>
2022-09-07 13:24:35 +0300

    Added icon and name

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.chatapp
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chatapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
+        initActionBar()
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
         binding.bSend.setOnClickListener {
@@ -35,5 +43,18 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun initActionBar() {
+        val bar = supportActionBar
+        Thread {
+            val bMap = Picasso.get().load(auth.currentUser?.photoUrl).get()
+            val dIcon = BitmapDrawable(resources, bMap)
+            runOnUiThread {
+                bar?.setDisplayHomeAsUpEnabled(true)
+                bar?.setHomeAsUpIndicator(dIcon)
+                bar?.title = auth.currentUser?.displayName
+            }
+        }.start()
     }
 }
